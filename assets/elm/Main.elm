0c904e373d58b1b0dc5dc43678ce298467476986
Wang Chen <wangcjen@gmail.com>
2017-03-08 19:54:15 -0600

    Add files via upload

--- assets/elm/Main.elm
+++ assets/elm/Main.elm
@@ -1,102 +1,327 @@
 module Main exposing (..)
 
+import Phoenix
+import Phoenix.Socket as Socket exposing (Socket)
+import Phoenix.Channel as Channel exposing (Channel)
+import Phoenix.Push as Push
+
+import Json.Encode as JE
+import Json.Decode as JD exposing (Decoder)
+
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
 
-type alias Model =
-  { view : String
-  , msg : String
-  }
+import Array exposing (Array, repeat, get)
 
-main : Program Model Model Never
+
+
+main : Program Flags Model Msg
 main = 
   programWithFlags 
-    { init = \ model -> ( model, Cmd.none )
-    , subscriptions = \ _ -> Sub.none
-    , update = \ _ -> \ model -> ( model, Cmd.none )
+    { init = init
     , view = view
+    , update = update
+    , subscriptions = subscriptions
     }
 
 
-view : Model -> Html Never
+
+-- MODEL
+
+
+type alias Flags = 
+  { view : String
+  , msg : String
+  , player: String
+  }
+
+
+type alias Visible =
+  { full : Bool
+  , game : Bool
+  , newgame : Bool
+  }
+
+
+type alias Stats =
+  { xName : String
+  , oName : String
+  , xScore : Int
+  , tiesScore : Int
+  , oScore : Int
+  }
+
+
+type alias Board =
+  { pieces: Array (Maybe String)
+  , next: String
+  }
+
+
+type alias Model =
+  { visible : Visible
+  , flags : Flags 
+  , stats : Stats
+  , board : Board
+  }
+
+
+init : Flags -> (Model, Cmd Msg)
+init flags =
+  let
+    visible = 
+      Visible True True True
+    stats = 
+      Stats "" "" 0 0 0
+    board = 
+      Board (repeat 9 Nothing) "x"
+  in 
+  (Model visible flags stats board, Cmd.none)
+
+
+
+-- UPDATE
+
+
+type Msg 
+  = OnJoinOk
+  | OnJoinError
+  | NewGame
+  | PutPiece String
+  | NewPlayer JD.Value
+  | PlayerLeft JD.Value
+  | UpdateBoard JD.Value
+  | NewRound JD.Value
+  | FinishGame JD.Value
+
+
+decodeBoard : Decoder Board
+decodeBoard = 
+  JD.map2 Board
+    (JD.at [ "board","data" ] (JD.array <| JD.nullable <| JD.string))
+    (JD.field "next" JD.string)
+
+
+updateBoard : JD.Value -> Maybe Board
+updateBoard resp =
+  case JD.decodeValue decodeBoard resp of
+    Ok board -> Just board
+    Err _ -> Nothing
+
+
+decodeStats : Decoder Stats
+decodeStats = 
+  JD.map5 Stats
+    (JD.field "x" JD.string)
+    (JD.field "o" JD.string)
+    (JD.at [ "score","x" ] JD.int)
+    (JD.at [ "score","ties" ] JD.int)
+    (JD.at [ "score","o" ] JD.int)
+
+
+updateStats : JD.Value -> Maybe Stats
+updateStats resp =
+  case JD.decodeValue decodeStats resp of
+    Ok stats -> Just stats
+    Err _ -> Nothing
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  let
+    { visible, flags, stats, board } = model
+  in
+    case msg of
+      OnJoinOk ->
+        model ! []
+      OnJoinError ->
+        { model | visible = { visible | full = False } } ! []
+      NewGame ->
+        let
+          push = 
+            Push.init ("game:" ++ model.flags.msg) "new_round"
+        in
+          model ! [ Phoenix.push echoServer push ]
+      PutPiece piece ->
+        let
+          push = 
+            Push.init ("game:" ++ model.flags.msg) "put"
+              |> Push.withPayload (JE.object [ ("index", JE.string piece) ])
+        in
+          model ! [ Phoenix.push echoServer push ]
+      NewPlayer resp ->
+        case (updateBoard resp, updateStats resp) of
+          (Just board, Just stats) ->
+            { model | visible = { visible | newgame = True, game = False }, board = board, stats = stats } ! []
+          _ ->
+            { model | visible = { visible | game = True } } ! []
+      PlayerLeft _ ->
+        { model | visible = { visible | newgame = True, game = True } } ! []
+      UpdateBoard resp ->
+        case updateBoard resp of
+          Just board ->
+            { model | board = board } ! []
+          _ ->
+            model ! []
+      NewRound resp ->
+        case updateBoard resp of
+          Just board ->
+            { model | visible = { visible | newgame = True }, board = board } ! []
+          _ ->
+            model ! []
+      FinishGame resp ->
+        case (updateBoard resp, updateStats resp) of
+          (Just board, Just stats) ->
+            { model | visible = { visible | newgame = False }, board = board, stats = stats } ! []
+          _ ->
+            model ! []
+
+
+
+-- SUBSCRIPTIONS
+
+
+echoServer : String
+echoServer =
+    "ws://localhost:4000/socket/websocket"
+
+
+socket : String -> Socket Msg
+socket player =
+  Socket.init echoServer
+    |> Socket.withParams [ ("player", player) ]
+      
+
+
+channel : String -> Channel Msg
+channel gameName = 
+  Channel.init ("game:" ++ gameName)
+    |> Channel.onJoin (\_ -> OnJoinOk)
+    |> Channel.onJoinError (\_ -> OnJoinError)
+    |> Channel.on "new_player" NewPlayer
+    |> Channel.on "player_left" PlayerLeft
+    |> Channel.on "update_board" UpdateBoard
+    |> Channel.on "new_round" NewRound
+    |> Channel.on "finish_game" FinishGame
+    |> Channel.withDebug
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  let
+    flags = model.flags
+  in
+    case flags.view of
+      "game_show" -> 
+        Phoenix.connect (socket flags.player) [ channel flags.msg ]
+      _ -> 
+        Sub.none
+  
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
 view model =
-  case model.view of
-    "game_show" -> boardView model
-    _ -> formView model
+  let
+    flags = model.flags
+  in
+    case flags.view of
+      "game_show" -> boardView model
+      _ -> formView flags
 
 
-boardView : Model -> Html Never
+pieceView : Int -> Array (Maybe String) -> Html Msg
+pieceView num pieces =
+  case get num pieces of
+    Just (Just value) -> text value
+    _ -> text ""
+
+
+boardView : Model -> Html Msg
 boardView model = 
+  let
+    { visible, flags, stats, board } = model
+    { pieces, next } = board
+  in
   div [] 
     [ div 
-      [ id "full_game", class "msg hidden" ] 
+      [ id "full", classList [ ("msg", True), ("hidden", visible.full) ] ] 
       [ text "Sorry, the game is already full :(" ]
     , div 
-      [ id "waiting", class "msg hidden" ] 
+      [ id "waiting", classList [ ("msg", True), ("hidden", not visible.game) ] ] 
       [ text "Waiting for a second player..." ]
     , table 
-      [ id "game", attribute "data-name" model.msg, class "hidden" ]
+      [ id "game", attribute "data-name" flags.msg, classList [ ("hidden", visible.game) ] ]
       [ tr [ class "top" ]
-        [ td [ id "index_0", class "left", attribute "data-index" "0" ] []
-        , td [ id "index_1", attribute "data-index" "1" ] []
-        , td [ id "index_2", class "right", attribute "data-index" "2" ] []
+        [ td [ id "index_0", class "left", onClick (PutPiece "0") ] [ pieceView 0 pieces ]
+        , td [ id "index_1", onClick (PutPiece "1") ] [ pieceView 1 pieces ]
+        , td [ id "index_2", class "right", onClick (PutPiece "2") ] [ pieceView 2 pieces ]
         ]
       , tr []
-        [ td [ id "index_3", class "left", attribute "data-index" "3" ] []
-        , td [ id "index_4", attribute "data-index" "4" ] []
-        , td [ id "index_5", class "right", attribute "data-index" "5" ] []
+        [ td [ id "index_3", class "left", onClick (PutPiece "3") ] [ pieceView 3 pieces ]
+        , td [ id "index_4", onClick (PutPiece "4") ] [ pieceView 4 pieces ]
+        , td [ id "index_5", class "right", onClick (PutPiece "5") ] [ pieceView 5 pieces ]
         ]
       , tr [ class "bottom" ]
-        [ td [ id "index_6", class "left", attribute "data-index" "6" ] []
-        , td [ id "index_7", attribute "data-index" "7" ] []
-        , td [ id "index_8", class "right", attribute "data-index" "8" ] []
+        [ td [ id "index_6", class "left", onClick (PutPiece "6") ] [ pieceView 6 pieces ]
+        , td [ id "index_7", onClick (PutPiece "7") ] [ pieceView 7 pieces ]
+        , td [ id "index_8", class "right", onClick (PutPiece "8") ] [ pieceView 8 pieces ]
         ]
       ]
     , div 
-      [ id "stats", class "hidden" ]
+      [ id "stats", classList [ ("hidden", visible.game) ] ]
       [ div 
         [ id "x", class "block" ]
         [ div 
           [ class "name" ]
           [ span
-            [ id "x_turn", class "turn" ]
-            [ text "&#8680;" ]
-          , span [ id "x_name" ] []
+            [ id "x_turn", classList [ ("turn", True), ("hidden", next /= "x") ] ]
+            [ text "⇨" ]
+          , span [ id "x_name" ] [ text stats.xName ]
           , span [] [ text "(x)" ]
           ]
-        , div [ id "x_score", class "score" ] []
+        , div [ id "x_score", class "score" ] [ text <| toString <| stats.xScore ]
         ]
       , div 
         [ id "ties", class "block" ] 
         [ div [ class "name" ] [ text "ties" ]
-        , div [ id "ties_score", class "score" ] []
+        , div [ id "ties_score", class "score" ] [ text <| toString <| stats.tiesScore ]
         ]
       , div 
         [ id "o", class "block" ]
         [ div 
           [ class "name" ]
-          [ span [ id "o_name" ] []
+          [ span [ id "o_name" ] [ text stats.oName ]
           , span [] [ text "(o)" ]
           , span
-            [ id "o_turn", class "turn" ]
-            [ text "&#8678;" ]
+            [ id "o_turn", classList [ ("turn", True), ("hidden", next /= "o") ] ]
+            [ text "⇦" ]
           ]
-        , div [ id "o_score", class "score" ] []
+        , div [ id "o_score", class "score" ] [ text <| toString <| stats.oScore ]
         ]
       ]
     , div 
       [ class "text-center" ] 
       [ button 
-        [ id "new_game", class "btn btn-primary hidden" ]
+        [ id "new_game"
+        , classList 
+          [ ("btn btn-primary", True)
+          , ("hidden", visible.newgame)
+          ]
+        , onClick NewGame 
+        ]
         [ text "NEW GAME" ] 
       ]
     ]
 
 
-formView : Model -> Html Never
-formView model =
+formView : Flags -> Html Msg
+formView flags =
   let
-    word = String.dropRight 4 model.view
+    word = String.dropRight 4 flags.view
     str1 = "/" ++ word ++ "s"
     str2 = word ++ "_name"
     str3 = word ++ "[name]"
@@ -114,7 +339,7 @@ formView model =
         [ input 
           [ name "_csrf_token"
           , type_ "hidden"
-          , value model.msg ] []
+          , value flags.msg ] []
         , div [ class "input-group" ]
           [ input 
             [ autofocus True
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,9 @@
 import "phoenix_html"
-import {Socket} from "phoenix"
-import Game from "./game"
-
-let socket = new Socket(
-  "/socket",
-  {params: {player: window.currentPlayer}}
-)
-
-let element = document.getElementById("game")
-if (element) {
-  Game.init(socket)
-}
 
 let elmDiv = document.getElementById('elm-main')
 if (elmDiv) {
   let view = elmDiv.getAttribute("view")
   let msg = elmDiv.getAttribute("msg")
-  Elm.Main.embed(elmDiv, {view: view, msg: msg})
+  Elm.Main.embed(elmDiv, 
+    {view: view, msg: msg, player: window.currentPlayer})
 }
